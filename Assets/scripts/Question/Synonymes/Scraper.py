64d85f1045434ae3c145d7f003d443cb68e5e465
nicolas beaudouin <nicolasbeaudouin@MacBook-Pro-de-nicolas-2.local>
2022-03-16 00:24:12 +0100

    [ADD] Scripts pour scrap + json avec les synonymes + Mots Communs

---
+++ Assets/scripts/Question/Synonymes/Scraper.py
@@ -0,0 +1,49 @@
+import requests
+import json
+import random
+import os.path
+
+
+if os.path.isfile('./synonymes/synonymes.json'):
+    with open('./synonymes/synonymes.json',"rb") as syn:
+        dico = json.load(syn)
+else:
+    dico = {}
+
+
+with open("./words.json","rb") as words:
+    json_words = json.load(words)
+
+long = len(json_words)
+
+for i in range(100):
+    mot = json_words[random.randint(0,long-1)]
+    while(mot in dico):
+        mot = json_words[random.randint(0,long-1)]
+
+    
+    url = f"https://dictionaryapi.com/api/v3/references/thesaurus/json/{mot}?key=735f8927-d471-46ef-985c-156e7d93280a" 
+    r = requests.get(url, allow_redirects=True)
+    json_string = json.loads(r.text)
+    try:
+        synonyms = json_string[0]["meta"]["syns"]
+        definition = json_string[0]["shortdef"]
+        dico[mot] = {"synonyms":synonyms,"definition":definition}
+    except:
+        try: 
+            url = f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{mot}?key=8a111a24-4bb6-4f3d-a6a5-8ac310d5175c" 
+            r = requests.get(url, allow_redirects=True)
+            json_def = json.loads(r.text)
+            synonyms = json_string[0]["meta"]["syns"]
+            definition = json_def[0]["shortdef"]
+            dico[mot] = {"synonyms":synonyms,"definition":definition}
+        except:
+            continue
+    
+
+
+open(f'./synonymes/synonymes.json', 'w+').write(json.dumps(dico))
+
+
+
+
