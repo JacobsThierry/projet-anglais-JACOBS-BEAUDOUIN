8376ce1dffde2244babffe90ce06a007606ced11
Thierry JACOBS <thierry.jacobs@telecomnancy.eu>
2022-03-21 08:45:21 +0100

    serialize

---
+++ Assets/scripts/Question/JSONObject.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using System;
+
+[System.Serializable]
+public class JSONObject
+{
+
+    public List<List<String>> synonyms;
+
+    public List<String> definition;
+
+
+}
--- Assets/scripts/Question/JsonLoader.cs
+++ Assets/scripts/Question/JsonLoader.cs
@@ -3,22 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
+using System;
+using System.Linq;
 
-public class JsonLoader{
+public class JsonLoader
+{
 
-    private SynonymesCommon synonymes = new SynonymesCommon();
 
-    public JsonLoader(){
-        
-        string text = File.ReadAllText("./Ressources/synonymes/synonymesCommon.json");  
-        synonymes.dico = JsonConvert.DeserializeObject<Dictionary<string, Dictionary< string,List< List<string> > > >>(text);
-        Debug.Log(synonymes.dico["after"]["definition"][0]);
-        
+
+    Dictionary<String, JSONObject> synonymes;
+
+    public JsonLoader()
+    {
+
+        string text = File.ReadAllText("./Ressources/synonymes/synonymesCommon.json");
+        synonymes = JsonConvert.DeserializeObject<Dictionary<String, JSONObject>>(text);
+        Debug.Log(synonymes);
+
     }
 
-    public string getCommonWords(){
-        return synonymes.getRandomWord();
+    public string getCommonWords()
+    {
 
+        return synonymes.["fish"].synonyms[0][0];
 
     }
 
